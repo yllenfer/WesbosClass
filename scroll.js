ad2977beeda1e00b680db19be04e4250fa8e47f8
yllenfer <fer19020@byui.edu>
2020-08-05 19:10:41 -0500

    terms

---
+++ scroll.js
@@ -0,0 +1,22 @@
+
+    const terms = document.querySelector('.terms-and-conditions');
+    const watch = document.querySelector ('.watch');
+    const button = document.querySelector ('.accept');
+
+
+    function obCallback(payload){
+        if (payload[0].intersectionRatio === 1){
+        button.disabled = false; 
+    }else {
+        button.disabled = true;
+        
+    }
+}
+    const ob = new IntersectionObserver(obCallback, { 
+    root: terms, 
+    treshold: 0.2,
+     });
+
+    ob.observe(terms.lastElementChild);
+
+
