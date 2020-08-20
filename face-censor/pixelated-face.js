a6bf6edf0512617b47fe5500f85d6be66127ed29
yllenfer <fer19020@byui.edu>
2020-08-20 13:55:34 -0500

    face-censorship that is not working bw

--- face-censor/pixelated-face.js
+++ face-censor/pixelated-face.js
@@ -1,25 +1,32 @@
-const video  = document.querySelector ('.webcam');
-
-const canvas  = document.querySelector ('.video');
+const video = document.querySelector('.webcam');
+const canvas = document.querySelector('.video');
 const ctx = canvas.getContext('2d');
-ctx.strokeStyle = '#ffc600';
-ctx.lineWidth = 2;
-const faceCanvas  = document.querySelector ('.face');
-const faceCtx = canvas.getContext('2d');
-
+const faceCanvas = document.querySelector('.face');
+const faceCtx = faceCanvas.getContext('2d');
 const faceDetector = new window.FaceDetector();
-console.log(video, canvas, faceCanvas, faceDetector);
+const optionsInputs = document.querySelectorAll(
+  '.controls input[type="range"]'
+);
 
-async function populateVideo(){
-    const stream = await navigator.mediaDevices.getUserMedia({
+const options = {
+  SIZE: 10,
+  SCALE: 1.35,
+};
 
-        video: {width: 1280, height: 720}
-    
+function handleOption(event) {
+  const { value, name } = event.currentTarget;
+  options[name] = parseFloat(value);
+}
+optionsInputs.forEach(input => input.addEventListener('input', handleOption));
 
-    });  
-   video.srcObject = stream;
-   await video.play();
 
+async function populateVideo() {
+  const stream = await navigator.mediaDevices.getUserMedia({
+    video: { width: 1280, height: 720 },
+  });
+  video.srcObject = stream;
+  await video.play();
+ 
   console.log(video.videoWidth, video.videoHeight);
   canvas.width = video.videoWidth;
   canvas.height = video.videoHeight;
@@ -27,24 +34,55 @@ async function populateVideo(){
   faceCanvas.height = video.videoHeight;
 }
 
-async function detect(){
+async function detect() {
   const faces = await faceDetector.detect(video);
-  console.log(faces.length);
-  faces.forEach(drawFace) 
+ 
+  faces.forEach(drawFace);
+  faces.forEach(censor);
   requestAnimationFrame(detect);
 }
 
-function drawFace(face){
-    const {width, height, top, left} = face.boundingBox;
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.strokeStyle = '#ffc600'
-    ctx.lineWidth = 2; 
-    ctx.strokeRect(left, top, width, height);
-    
+function drawFace(face) {
+  const { width, height, top, left } = face.boundingBox;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.strokeStyle = '#ffc600';
+  ctx.lineWidth = 2;
+  ctx.strokeRect(left, top, width, height);
 }
 
-function censor({boundingBox}){
-    boundingBox;
+function censor({ boundingBox: face }) {
+  faceCtx.imageSmoothingEnabled = false;
+  faceCtx.clearRect(0, 0, faceCanvas.width, faceCanvas.height);
+ 
+  faceCtx.drawImage(
+   
+    video, 
+    face.x, 
+    face.y,
+    face.width,
+    face.height,
+    // 4 draw args
+    face.x, // where should we start drawing the x and y?
+    face.y,
+    options.SIZE,
+    options.SIZE
+  );
+  // draw the small face back on, but scale up
+
+  const width = face.width * options.SCALE;
+  const height = face.height * options.SCALE;
+  faceCtx.drawImage(
+    faceCanvas, // source
+    face.x, // where do we start the source pull from?
+    face.y,
+    options.SIZE,
+    options.SIZE,
+    // Drawing args
+    face.x - (width - face.width) / 2,
+    face.y - (height - face.height) / 2,
+    width,
+    height
+  );
 }
 
 populateVideo().then(detect);
